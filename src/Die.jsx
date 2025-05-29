f302834ebf8fdf1bf42dd2942d5334d14adfd8cd
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-05-29 14:47:10 +0200

    added dice faces

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './index.css';
 
+const diceChars = '⚀⚁⚂⚃⚄⚅';
+
 export default function Die(props) {
   const styles = {
     backgroundColor: props.isHeld ? '#59E391' : 'white',
   };
   return (
     <div className="die-face" style={styles} onClick={props.holdDice}>
-      <h2 className="die-num">{props.value}</h2>
+      <h2 className="die-num">{diceChars[props.value - 1]}</h2>
     </div>
   );
 }
